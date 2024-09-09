adf189cb63dae84be1c4c37d8645330f51c20c7d
Hsuan0223x <hsuan0223x@gmail.com>
2024-09-09 17:27:42 +0800

    Update 0909

--- 0909/1.py
+++ 0909/1.py
@@ -2,3 +2,5 @@ print("Hello World!")
 
 name = input("What is your name?\n")
 print("Hi,",name)
+
+# 基本輸入輸出，基本的print變數代入
---
+++ 0909/2.py
@@ -0,0 +1,6 @@
+# 練習
+PI = 3.14159
+r = float(input("請輸入半徑："))
+g = 2*PI*r
+a = r*r*PI
+print("圓面積為",a,"圓周長為",g)
